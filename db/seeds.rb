5ef607202a13a550d441fdd6305302ae7bb98685
nTraum <philipp.press@googlemail.com>
2014-02-06 19:41:24 +0100

    added seed for game modes and tf2 classes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+unless Tf2Class.any?
+  tf2_classes = %w[Scout Soldier Pyro Demoman Heavy Engineer Medic Sniper Spy]
+  tf2_classes.each { |tf2_class| Tf2Class.create(:name => tf2_class) }
+end
+
+unless GameMode.any?
+  game_modes = %w[6v6 Highlander Jump BBall]
+  game_modes.each { |game_mode| GameMode.create(:name => game_mode) }
+end
