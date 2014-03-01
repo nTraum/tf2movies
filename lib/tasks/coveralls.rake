9cdd2dff4d2cfde892c14d4929a09b53c91e2a86
nTraum <philipp.press@googlemail.com>
2014-03-01 15:09:39 +0100

    use normal rake task, as test coverage report seems to be merged

--- lib/tasks/coveralls.rake
+++
@@ -1,4 +0,0 @@
-task :test_with_coveralls => [:test, 'coveralls:push'] do
-  require 'coveralls/rake/task'
-  Coveralls::RakeTask.new
-end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
 ENV['RAILS_ENV'] = 'test'
 
 require 'coveralls'
-Coveralls.wear_merged!('rails')
+Coveralls.wear!('rails')
 
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
