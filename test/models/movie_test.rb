19e71fb3250bc373cda3338622f248db8ac24e31
nTraum <philipp.press@googlemail.com>
2014-03-12 15:45:53 +0100

    ignore key query parameter in vcr cassette, hopefully fixing broken travis ci builds

--- test/models/movie_test.rb
+++ test/models/movie_test.rb
@@ -8,12 +8,12 @@ describe Movie do
   end
 
   it 'must update meta data when being refreshed' do
-    VCR.use_cassette('movie_test_refresh_info') do
+    VCR.use_cassette('movie_test_refresh_info', :match_requests_on => [:method, VCR.request_matchers.uri_without_param(:key)]) do
       subject.refresh_info
     end
     subject.title.must_equal 'TF2 - 7 brothers (broder) frag movie'
     subject.description.must_equal 'Description'
-    subject.views.must_equal 105293
+    subject.views.must_equal 105304
     subject.info_refreshed_at.must_be_within_delta(Time.current, 3)
   end
 end
