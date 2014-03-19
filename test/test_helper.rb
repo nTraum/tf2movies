c1c1fb42c2fb994192d018e81107228d9dcb6920
nTraum <philipp.press@googlemail.com>
2014-03-19 11:19:27 +0100

    report back to coveralls

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,11 @@ ENV['RAILS_ENV'] = 'test'
 
 require 'simplecov'
 require 'coveralls'
+
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[
+  SimpleCov::Formatter::HTMLFormatter,
+  Coveralls::SimpleCov::Formatter
+]
 SimpleCov.start('rails') do
   add_group 'Policies', 'app/policies'
 end
