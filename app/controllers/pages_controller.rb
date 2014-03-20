262c22f0d2a63ac77f51fba09e18144b4f21c3b4
nTraum <philipp.press@googlemail.com>
2014-03-20 17:45:42 +0100

    progress with the welcome page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,12 @@ class PagesController < ApplicationController
   end
 
   def welcome
+    @movies_count       = Movie.count
+    @downloads_count    = Download.count
+    @authors_count      = Author.count
+    @total_duration     = Movie.sum(:duration)
+    @most_recent_movies = Movie.order(:created_at => :desc).limit(5)
+    @most_viewed_movies = Movie.order(:views => :desc).limit(5)
   end
 
   def contact
