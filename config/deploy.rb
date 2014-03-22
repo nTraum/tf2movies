4f6d10314d5d5795f95a004af59a7b3a6993e94c
nTraum <philipp.press@googlemail.com>
2014-03-22 03:39:10 +0100

    notify new relic of deploys

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,6 +36,7 @@ set :linked_dirs, %w{tmp/pids tmp/sockets log}
 # set :keep_releases, 5
 
 namespace :deploy do
+  before :finished, 'newrelic:notice_deployment'
 
   desc 'Restart application'
   task :restart do
