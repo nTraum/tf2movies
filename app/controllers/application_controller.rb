2e682167f1b3bca9d7cd79c225a1098d25cf0938
nTraum <philipp.press@googlemail.com>
2014-03-27 21:04:23 +0100

    remove user_id in session when we cant find the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,11 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    begin
+      @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    rescue ActiveRecord::RecordNotFound
+      session.delete :user_id
+    end
   end
 
   def user_not_authorized
