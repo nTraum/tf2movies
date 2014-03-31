6ddf8969699c7c1c447ff0a42fe28843d7b7fb12
nTraum <philipp.press@googlemail.com>
2014-03-31 20:49:19 +0200

    test response code as well

--- test/controllers/comments_controller_test.rb
+++ test/controllers/comments_controller_test.rb
@@ -5,6 +5,7 @@ describe CommentsController do
     as_logged_in_user do
       movie = FactoryGirl.create :movie
       post :create, { :movie_id => movie.id, :comment => FactoryGirl.attributes_for(:comment) }
+      response.status.must_equal 302
       flash[:notice].wont_be_nil
       movie.comments.size.must_equal 1
     end
