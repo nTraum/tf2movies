d4c4e2a2ca601fb22d221cbfd36553c59c92948f
nTraum <philipp.press@googlemail.com>
2014-04-16 02:32:52 +0200

    deploy.rb cap file is cool with 3.2.0

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for Capistrano 3.2
+lock '3.2.0'
 
 set :application, 'tf2movies'
 set :repo_url, 'https://github.com/nTraum/tf2movies.git'
