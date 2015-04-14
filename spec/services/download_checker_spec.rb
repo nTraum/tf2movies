f1ce933c984959512222ece07bd7ca28360ca14c
nTraum <philipp.press@googlemail.com>
2015-04-14 23:03:36 +0200

    download checker spec

--- spec/services/download_checker_spec.rb
+++ spec/services/download_checker_spec.rb
@@ -9,15 +9,39 @@ describe DownloadChecker do
     end
 
     context "when the HTTP response is 200 and has a content length" do
-      let (:content_length) { 123 }
+      let (:status) { 200 }
 
       before do
-        stub_request(:head, download.url).to_return(headers: { "Content-Length" => content_length })
+        stub_request(:head, download.url).to_return(status: status)
         described_class.check_status!(download)
       end
 
       it "sets the download to online" do
         expect(download).to be_online
+      end
+    end
+
+    context "when the HTTP response is 404" do
+      let(:status) { 404 }
+
+      before do
+        stub_request(:head, download.url).to_return(status: status)
+        described_class.check_status!(download)
+      end
+
+      it "sets the download to offline" do
+        expect(download).to be_offline
+      end
+    end
+
+    context "when the HTTP request times out" do
+      before do
+        stub_request(:head, download.url).to_timeout
+        described_class.check_status!(download)
+      end
+
+      it "sets the download to offline" do
+        expect(download).to be_unknown
       end
     end
   end
