157e5829593a3dd1944037042ed26aacfc3d75ff
nTraum <philipp.press@googlemail.com>
2015-04-14 23:07:45 +0200

    DRY download checker spec

--- spec/services/download_checker_spec.rb
+++ spec/services/download_checker_spec.rb
@@ -4,19 +4,15 @@ describe DownloadChecker do
   let(:download) { build(:download) }
 
   describe ".check_status!" do
-    after do
-      expect(download.status_refreshed_at).to be_within(2).of(Time.zone.now)
-    end
+    after { expect(download.status_refreshed_at).to be_within(2).of(Time.zone.now) }
 
     context "when the HTTP response is 200 and has a content length" do
       let (:status) { 200 }
 
-      before do
-        stub_request(:head, download.url).to_return(status: status)
-        described_class.check_status!(download)
-      end
+      before { stub_request(:head, download.url).to_return(status: status) }
 
       it "sets the download to online" do
+        described_class.check_status!(download)
         expect(download).to be_online
       end
     end
@@ -24,23 +20,19 @@ describe DownloadChecker do
     context "when the HTTP response is 404" do
       let(:status) { 404 }
 
-      before do
-        stub_request(:head, download.url).to_return(status: status)
-        described_class.check_status!(download)
-      end
+      before { stub_request(:head, download.url).to_return(status: status) }
 
       it "sets the download to offline" do
+        described_class.check_status!(download)
         expect(download).to be_offline
       end
     end
 
     context "when the HTTP request times out" do
-      before do
-        stub_request(:head, download.url).to_timeout
-        described_class.check_status!(download)
-      end
+      before { stub_request(:head, download.url).to_timeout }
 
       it "sets the download to offline" do
+        described_class.check_status!(download)
         expect(download).to be_unknown
       end
     end
